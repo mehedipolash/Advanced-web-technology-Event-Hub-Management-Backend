23a72d218fb1f8efaffac7dd9578130d1328a022
mehedipolash <mehedipolash228@gmail.com>
2025-09-08 10:16:50 +0600

    react toast added.navbar/footer updated

--- Frontend/next-task-1/app/components/Navbar.tsx
+++ Frontend/next-task-1/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-teal-600 text-white p-4 flex justify-between">
+    <nav className="bg-teal-900 text-white p-4 flex justify-between">
       <h1 className="font-bold">EventHub</h1>
       <div className="space-x-4">
         <Link href="/">Home</Link>
